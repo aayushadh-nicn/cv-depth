18b0699cb25619008d2052dfc0d02b375588ea7a
aayushadh-nicn <82963053+aayushadh-nicn@users.noreply.github.com>
2021-04-21 22:12:37 +0545

    Add files via upload
    Uploading CV-depth files

---
+++ detectdistance.py
@@ -0,0 +1,30 @@
+import cv2
+import pyrealsense2
+from realsense_depth import *
+x = int(input("Enter x axis ="))
+y = int(input("Enter y axis = "))
+point = (x, y)
+
+# Initialize Camera Intel Realsense
+dc = Depth_Camera()
+def show_distance(event, x, y, args, params):
+    global point
+    point = (x, y)
+# Create mouse event
+cv2.namedWindow("Original Color Frame")
+cv2.setMouseCallback("Original Color Frame", show_distance)
+
+while True:
+    ret, depth_frame, color_frame = dc.get_frame()
+
+    # Show distance for a specific point
+    cv2.circle(color_frame, point, 4, (0, 0, 255))
+    distance = depth_frame[point[1], point[0]]
+
+    cv2.putText(color_frame, "distance = {}mm".format(distance), (point[0], point[1] - 20), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
+
+    cv2.imshow("depth frame", depth_frame)
+    cv2.imshow("Color frame", color_frame)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
